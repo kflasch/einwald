d470fa544bc726fbb424c652067220b707ad97c0
Kevin Flasch <kflasch@gmail.com>
2015-12-09 23:45:10 -0600

    player entity

--- entity.js
+++ entity.js
@@ -13,20 +13,50 @@ Game.Entity = function(properties) {
 
 Game.Entity.extend(Game.DynamicGlyph);
 
+Game.Entity.prototype.setPosition = function(x, y) {
+    var oldX = this._x;
+    var oldY = this._y;
+
+    this._x = x;
+    this._y = y;
+
+    if (this._zone)
+        this._zone.updateEntityPosition(this, oldX, oldY);
+};
+
+// todo: combine player with entities, zone arg?
 Game.Entity.prototype.tryMove = function(x, y, zone) {
-    var tile = zone.getTile(x, y);
-    if (tile._passable) {
-        this._x = x;
-        this._y = y;
-        Game.message = 'You pass through ' + tile._desc + '.';
+    var tile = this._zone.getTile(x, y);
+    var isPlayer = this.hasMixin(Game.EntityMixins.PlayerActor);
+    var descMsg = null;
+    var target = this._zone.getEntityAt(x, y);
+    if (target) {
+        if (isPlayer) {
+            descMsg = "You touch " + target._name + ".";
+        }
+    } else if (tile._passable) {
+        this.setPosition(x, y);
+
+        var items = this._zone.getItemsAt(x, y);
+        if (items) {
+            if (items.length === 1) {
+                descMsg = "You see a " + items[0].describe() + ".";
+            } else {
+                descMsg = "You see several objects here.";
+            }
+        } else {
+             descMsg = 'You pass through ' + tile._desc + '.';
+        }
     } else {
         if (tile == Tile.nullTile) {
-            Game.message = "You cannot pass this way.";
+             descMsg = "You cannot pass this way.";
         } else {
-            Game.message = (tile._desc || 'Something') + ' is in the way.';
-            Game.message = Game.message.capitalize();
+             descMsg = (tile._desc || 'Something') + ' is in the way.';
+             descMsg = descMsg.capitalize();
         }
     }
+    if (isPlayer && descMsg)
+        Game.message = descMsg;
 };
 
 // mixins
@@ -40,11 +70,7 @@ Game.EntityMixins.PlayerActor = {
         Game.turns++;
         Game.refresh();
         Game.engine.lock();
-    },
-    draw: function() {
-        Game.display.draw(this._x, this._y, "@", "#ff0");
     }
-
 };
 
 Game.EntityMixins.InventoryHolder = {
@@ -84,13 +110,49 @@ Game.EntityMixins.InventoryHolder = {
     }
 };
 
+Game.EntityMixins.TaskActor = {
+    name: 'TaskActor',
+    groupName: 'Actor',
+    init: function(template) {
+        this._tasks = template['tasks'] || ['wander'];
+    },
+    act: function() {
+        for (var i = 0; i < this._tasks.length; i++) {
+            if (this.canDoTask(this._tasks[i])) {
+                this[this._tasks[i]]();
+                return;
+            }
+        }
+    },
+    canDoTask: function(task) {
+        return true;
+    },
+    wander: function() {
+        var moveOffset = (Math.round(Math.random()) === 1) ? 1 : -1;
+        if (Math.round(Math.random()) === 1) {
+            this.tryMove(this._x + moveOffset, this._y);
+        } else {
+            this.tryMove(this._x, this._y + moveOffset);
+        }
+    }
+};
 
 // entities
 
 Game.PlayerTemplate = {
     name: 'player',
-    character: '@',
-    foreground: '#ff0',
+    chr: '@',
+    fg: '#ff0',
     mixins: [Game.EntityMixins.PlayerActor,
              Game.EntityMixins.InventoryHolder]
 };
+
+
+Game.EntityRepository = new Game.Repository('entities', Game.Entity);
+
+Game.EntityRepository.define('sb', {
+    name: 'sb',
+    chr: 's',
+    fg: 'red',
+    mixins: [Game.EntityMixins.TaskActor]
+});
--- zone.js
+++ zone.js
@@ -5,8 +5,9 @@ Game.Zone = function(tiles) {
     this._width =  tiles.length || Game.mapWidth;
     this._height = tiles[0].length || Game.mapHeight;
 
-    // map of 'x,y' to items
+    // map of 'x,y' to items, entities
     this._items = {};
+    this._entities = {};
 };
 
 Game.Zone.prototype.getTile = function(x, y) {
@@ -33,7 +34,83 @@ Game.Zone.prototype.setItemsAt = function(x, y, items) {
     }
 };
 
-Game.Zone.Forest = function(tiles) {
+Game.Zone.prototype.getEmptyRandomPosition = function() {
+    var x, y;
+    do {
+        x = Math.floor(ROT.RNG.getUniform() * this._width);
+        y = Math.floor(ROT.RNG.getUniform() * this._height);
+    } while (!this.isPassable(x,y));
+    return {x: x, y: y};
+};
+
+// true if given pos is passable and has no entities
+Game.Zone.prototype.isPassable = function(x, y) {
+    return this.getTile(x, y)._passable &&
+        !this.getEntityAt(x, y);
+    /*
+    if (this.zone._tiles[x] === undefined ||
+        this.zone._tiles[x][y] === undefined ||
+        this.zone._tiles[x][y]._passable == false) {
+        return false;
+    } else {
+        return true;
+    }
+    */
+};
+
+Game.Zone.prototype.getEntityAt = function(x, y) {
+    return this._entities[x + ',' + y];
+};
+
+Game.Zone.prototype.addEntityAtRandomPosition = function(entity) {
+    var pos = this.getEmptyRandomPosition();
+    entity._x = pos.x;
+    entity._y = pos.y;
+    this.addEntity(entity);
+};
+
+Game.Zone.prototype.addEntity = function(entity) {
+    entity._zone = this;
+
+    this.updateEntityPosition(entity);
+
+    if (entity.hasMixin('Actor')) {
+        Game.scheduler.add(entity, true);
+    }
+
+    //if (entity.hasMixin('PlayerActor')) {
+    //    Game.player = entity;
+    //}
+
+};
+
+Game.Zone.prototype.updateEntityPosition = function(entity, oldX, oldY) {
+
+    // if coords passed in, deleted old key 
+    if (typeof oldX === 'number') {
+        var oldKey = oldX + ',' + oldY;
+        if (this._entities[oldKey] == entity) {
+            delete this._entities[oldKey];
+        }
+    }
+    
+    if (entity._x < 0 || entity._x >= this._width ||
+        entity._y < 0 || entity._y >= this._height) {
+        throw new Error("Entity's position is out of bounds.");
+    }
+
+    var key = entity._x + ',' + entity._y;
+    if (this._entities[key]) {
+        throw new Error('Tried to add entity to location with another entity.');
+    }
+
+    this._entities[key] = entity;
+};
+
+
+
+
+Game.Zone.Forest = function(tiles, player) {
 
     Game.Zone.call(this, tiles);
 
@@ -48,10 +125,21 @@ Game.Zone.Forest = function(tiles) {
         }
     }.bind(this));
 
-    x = Math.floor(ROT.RNG.getUniform() * (Game.mapWidth - 2));
-    y = Math.floor(ROT.RNG.getUniform() * (Game.mapHeight - 2));
-    var key = x + ',' + y;
-    this._items[key] = [Game.ItemRepository.createRandom()];
+    this.addEntityAtRandomPosition(player);
+
+    var entity = Game.EntityRepository.createRandom();
+    this.addEntityAtRandomPosition(entity);
+
+    for (var i=0; i<20; i++) {
+        x = Math.floor(ROT.RNG.getUniform() * (Game.mapWidth - 2));
+        y = Math.floor(ROT.RNG.getUniform() * (Game.mapHeight - 2));
+        if (this.getTile(x, y)._passable) {
+            var key = x + ',' + y;
+            this._items[key] = [Game.ItemRepository.createRandom()];
+        }
+    }
+
+
 };
 
 Game.Zone.Forest.extend(Game.Zone);
